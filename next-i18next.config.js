1f697deb55d31c5f0b89d2ed2af4db81c0f16a2f
Nguyen Huu Trung <huutrung.mmt@gmailc.om>
2023-02-17 10:22:10 +0700

    chore: remove dependency unused

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,5 +2,9 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'vi'],
+    fallbackLng: 'en',
+    // reloadOnPrerender option
+    // next-i18next will reload our translations when we make changes to our translation files.
+    reloadOnPrerender: process.env.NODE_ENV === 'development',
   },
 }
