1dbd5f55cec284c70ec92437b139947432d2b7c5
Nguyen Huu Trung <huutrung.mmt@gmailc.om>
2023-02-18 11:01:04 +0700

    feat: keep on

---
+++ src/components/UI/shared/Language.tsx
@@ -0,0 +1,36 @@
+import clsx from 'clsx'
+import { useRouter } from 'next/router'
+
+import { LanguageName } from '@/enums/languageName'
+import { useChangeLanguage } from '@/hooks/useChangeLanguage'
+
+interface LanguageProps {
+  icon?: string
+}
+
+export default function Language({ icon }: LanguageProps) {
+  const router = useRouter()
+  const { change } = useChangeLanguage()
+
+  return (
+    <>
+      <i
+        className={clsx({
+          'mr-2.5 text-2xl': true,
+          [`${icon}`]: !!icon,
+        })}
+      />
+      <select
+        onChange={change}
+        value={router.locale}
+        className="change-language group flex items-center bg-white text-secondary outline-none">
+        <option value="en" className="group-hover:underline">
+          {LanguageName.English}
+        </option>
+        <option value="vi" className="group-hover:underline">
+          {LanguageName.Vietnam}
+        </option>
+      </select>
+    </>
+  )
+}
--- src/components/UI/shared/Menu.tsx
+++ src/components/UI/shared/Menu.tsx
@@ -1,19 +1,19 @@
+import clsx from 'clsx'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 import { MouseEvent, useRef } from 'react'
 
+import Language from '@/components/UI/shared/Language'
+import { menu } from '@/data/menu'
 import { DeviceDemension } from '@/enums/deviceDemension'
-import { LanguageName } from '@/enums/languageName'
-import { useChangeLanguage } from '@/hooks/useChangeLanguage'
 
 export default function Menu() {
   const articleRef = useRef<HTMLAnchorElement>(null)
   const dropdownMenuRef = useRef<HTMLUListElement>(null)
 
-  const router = useRouter()
+  const { pathname } = useRouter()
   const { t } = useTranslation('header')
-  const { change } = useChangeLanguage()
 
   const onToggleMenu = (event: MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault()
@@ -25,75 +25,50 @@ export default function Menu() {
 
   return (
     <ul className="block lg:flex">
-      <li className="ml-0 mr-5">
-        <Link href="/" className="active-link" title={t<string>('header_home')}>
-          {t('header_home')}
-        </Link>
-      </li>
-      <li className="group relative mx-5">
-        <Link
-          href="#"
-          onClick={(event) => onToggleMenu(event)}
-          className="dropdown-toggle light-link pb-10 after:absolute after:top-[calc(50%-18px)] after:-ml-0.5 after:font-icons after:text-24px after:transition-all after:content-['\e688'] lg:group-hover:after:rotate-180"
-          title={t<string>('header_blog_articles')}>
-          {t('header_blog_articles')}
-        </Link>
-        <ul className="dropdown-menu hidden lg:group-hover:block" ref={dropdownMenuRef}>
-          <li className="after:bg-blueColor">
-            <Link href="/blog-list" title="Blog CSS articles">
-              CSS
-            </Link>
+      {menu.length &&
+        menu.map((item, index) => (
+          <li
+            key={index}
+            className={clsx({
+              'ml-0 mr-5': index === 0,
+              'mx-5': index > 0,
+              'group relative': index === 1,
+            })}>
+            {!item.group ? (
+              <Link
+                href={item.path}
+                className={clsx({
+                  'light-link': true,
+                  [item.activeLink]: pathname === item.path,
+                })}
+                title={t<string>(item.target)}>
+                {t(item.target)}
+              </Link>
+            ) : (
+              <>
+                <Link
+                  href="#"
+                  onClick={(event) => onToggleMenu(event)}
+                  className="dropdown-toggle light-link pb-10 after:absolute after:top-[calc(50%-18px)] after:-ml-0.5 after:font-icons after:text-24px after:transition-all after:content-['\e688'] lg:group-hover:after:rotate-180"
+                  title={item.title}>
+                  {t(item.target)}
+                </Link>
+                <ul className="dropdown-menu hidden lg:group-hover:block" ref={dropdownMenuRef}>
+                  {item.group.length &&
+                    item.group.map((group, key) => (
+                      <li key={key} className={group.backgroundCss as string}>
+                        <Link href={group.path} title={group.title}>
+                          {t(group.target)}
+                        </Link>
+                      </li>
+                    ))}
+                </ul>
+              </>
+            )}
           </li>
-          <li className="after:bg-redColor">
-            <Link href="/blog-list" title="Blog HTML articles">
-              HTML
-            </Link>
-          </li>
-          <li className="after:bg-yellowColor">
-            <Link href="/blog-list" title="Blog Javascript articles">
-              Javascript
-            </Link>
-          </li>
-          <li className="after:bg-greenColor">
-            <Link href="/blog-list" title="Graphic">
-              Graphic
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog-list" title={t<string>('header_blog_posts')}>
-              {t('header_blog_posts')}
-            </Link>
-          </li>
-        </ul>
-      </li>
-      <li className="mx-5">
-        <Link href="/about-me" className="light-link" title={t<string>('header_about')}>
-          {t('header_about')}
-        </Link>
-      </li>
-      <li className="mx-5">
-        <Link href="/portfolio" className="light-link" title={t<string>('header_my_projects')}>
-          {t('header_my_projects')}
-        </Link>
-      </li>
-      <li className="mx-5">
-        <Link href="/contact" className="light-link" title={t<string>('header_contact')}>
-          {t('header_contact')}
-        </Link>
-      </li>
+        ))}
       <li className="buyproducts-link ml-auto flex">
-        <i className="pe-7s-bookmarks mr-2.5 text-2xl"></i>
-        <select
-          onChange={change}
-          value={router.locale}
-          className="change-language group flex items-center bg-white text-secondary outline-none">
-          <option value="en" className="group-hover:underline">
-            {LanguageName.English}
-          </option>
-          <option value="vi" className="group-hover:underline">
-            {LanguageName.Vietnam}
-          </option>
-        </select>
+        <Language icon="pe-7s-bookmarks" />
       </li>
     </ul>
   )
