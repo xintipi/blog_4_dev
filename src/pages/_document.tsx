0777f7e38489f5479b19ed45da464b539217733c
Nguyen Huu Trung <huutrung.mmt@gmailc.om>
2023-02-19 09:17:23 +0700

    feat: keep on

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,11 @@ export default function Document() {
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link
+          href={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GA_TRACKING_ID}`}
+          rel="preload"
+          as="script"
+        />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com/" />
       </Head>
       <body className="bg-bodyBg font-sans text-bodyColor">
