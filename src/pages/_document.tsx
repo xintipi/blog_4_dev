5822a01e8f4f268306193190319acf0c0e3006ee
Nguyen Huu Trung <huutrung.mmt@gmailc.om>
2023-02-19 09:14:04 +0700

    feat: keep on

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,9 @@ export default function Document() {
           name="google-site-verification"
           content={process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION}
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com/" />
       </Head>
       <body className="bg-bodyBg font-sans text-bodyColor">
