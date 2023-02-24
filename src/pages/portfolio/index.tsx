025382204ace4809200597a1737613eda9fd16f2
Nguyen Huu Trung <huutrung.mmt@gmailc.om>
2023-02-24 17:40:48 +0700

    fix: bug fix

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,12 +1,12 @@
-import { GetStaticPropsContext } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-export async function getStaticProps(context: GetStaticPropsContext) {
-  return {
-    redirect: {
-      destination: '/portfolio/all',
-      permanent: false,
-    },
-  }
+export default function Portfolio() {
+  const router = useRouter()
+  useEffect(() => {
+    const { pathname } = router
+    if (pathname == '/portfolio') {
+      router.push('/portfolio/all').then((r) => r)
+    }
+  })
 }
-
-export default function Portfolio() {}
