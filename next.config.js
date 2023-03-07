a7194a162ab894ff28db9f814326482edc0e6e35
Nguyen Huu Trung <huutrung.mmt@gmailc.om>
2023-03-07 16:29:59 +0700

    fix: bugsfix

--- next.config.js
+++ next.config.js
@@ -24,23 +24,6 @@ const nextConfig = {
     domains: ['i.ibb.co'],
   },
   outputFileTracing: false,
-  async headers() {
-    return [
-      {
-        source: '/api/(.*)',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'https://dblog.one' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = withPlaiceholder(nextConfig)
